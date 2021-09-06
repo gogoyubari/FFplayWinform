0cfd5de6f0929837f3354a1c1ee28503ab3365db
gogoyubari <1097517+gogoyubari@users.noreply.github.com>
2021-09-06 12:06:26 +0900

    first commit

---
+++ FFplayWinform/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FFplayWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(720, 405);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(0, 404);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(720, 20);
+            this.panel2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(720, 425);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "bmd_h264_cat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ FFplayWinform/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Drawing.Text;
+using System.Text.RegularExpressions;
+using System.Configuration;
+using Microsoft.Win32;
+using System.Windows.Forms.VisualStyles;
+
+namespace FFplayWinform
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        public Form1()
+        {
+            InitializeComponent();
+            Application.EnableVisualStyles();
+            this.DoubleBuffered = true;
+        }
+
+        public Process ffplay1 = new Process();
+        public Process ffplay2 = new Process();
+        private void FFplay()
+        {
+            ffplay1.StartInfo.FileName = @"ffplay.exe";
+            ffplay1.StartInfo.Arguments = $@"-fflags nobuffer -analyzeduration 500000 -i udp://224.1.1.1:10001?timeout=3000000 -top 2000 -left 0 -x 720 -noborder";
+            ffplay1.StartInfo.CreateNoWindow = true;
+            ffplay1.StartInfo.UseShellExecute = false;
+            ffplay1.Start();
+
+            ffplay2.StartInfo.FileName = @"ffplay.exe";
+            ffplay2.StartInfo.Arguments = $@"-fflags nobuffer -analyzeduration 500000 -f lavfi -i amovie=udp\\://224.1.1.1\\:10001?timeout=3000000,showvolume=f=0:w=720:h=10:dm=1:p=1 -top 2000 -left 0 -noborder";
+            ffplay2.StartInfo.CreateNoWindow = true;
+            ffplay2.StartInfo.UseShellExecute = false;
+            ffplay2.Start();
+
+            Thread.Sleep(3000);
+            
+            SetParent(ffplay1.MainWindowHandle, panel1.Handle);
+            MoveWindow(ffplay1.MainWindowHandle, 0, 0, 720, 405, true);
+
+            SetParent(ffplay2.MainWindowHandle, panel2.Handle);
+            MoveWindow(ffplay2.MainWindowHandle, 0, 0, 720, 20, true);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FFplay();
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                ffplay1.CloseMainWindow();
+                ffplay2.CloseMainWindow();
+                ffplay1.WaitForExit(5000);
+                ffplay2.WaitForExit(5000);
+            }
+            catch { }
+        }
+    }
+}
